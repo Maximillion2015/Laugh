9b6e1ba9fbb1d0961fb527e4e2de38df76bdb802
Maximillion2015 <15620602691@163.com>
2016-03-29 13:16:12 +0800

    Initial Commit

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 张艺峰 on 16/3/29.
+//  Copyright © 2016年 张艺峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
